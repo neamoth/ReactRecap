ceaf3842c43c6443528c14d421bb9d101c8584ba
neamoth <neamoth15-8202@diu.edu.bd>
2025-01-29 16:35:35 +0600

    Creating first static component

--- reactInitial/src/index.jsx
+++ reactInitial/src/index.jsx
@@ -4,6 +4,18 @@ const root  = createRoot(document.querySelector("#root"));
 const reactElement = <h1>Hello im from jsx!</h1>
 console.log(reactElement);
 
+function CreateList(){
+  return(
+    <ul>
+      <li>Mango</li>
+      <li>Banana</li>
+      <li>Pinaple</li>
+    </ul>
+  );
+}
+
 root.render(
-  reactElement
+  <div>
+    <CreateList/>
+  </div>
 );
