f8fdeebecd92136b8a6e1de56a52589d7eb2e729
neamoth <neamoth15-8202@diu.edu.bd>
2025-01-29 16:24:21 +0600

    createElement Continue

--- reactInitial/src/index.jsx
+++ reactInitial/src/index.jsx
@@ -2,6 +2,9 @@ import { createElement } from "react";
 import { createRoot } from "react-dom/client";
 
 const root  = createRoot(document.querySelector("#root"));
+const reactElement = createElement("h1", null, "Hello I am from CreatElements")
+console.log(reactElement);
+
 root.render(
-  createElement("h1", null, "Hello I am from CreatElements")
+  reactElement
 );
